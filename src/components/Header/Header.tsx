9123927d26fd8f4028865f5460cff78a9eb2c040
Syed Ali <sydali3103@gmail.com>
2025-02-09 19:15:55 +0530

    Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,44 +7,57 @@ const navItems = [
   {
     name: "Home",
     link: "/",
+    hidden: true,
   },
   {
     name: "Services",
     link: "/services",
+    hidden: true,
   },
   {
     name: "Courses",
     link: "/courses",
+    hidden: true,
   },
   {
     name: "Case Studies",
     link: "/case-studies",
+    hidden: true,
   },
   {
     name: "Blogs",
     link: "/blogs",
+    hidden: true,
   },
   {
     name: "Contact",
     link: "/contact",
+    hidden: true,
   },
 ];
 
 const Header: React.FC = () => {
   return (
-    <header className={styles.header}>
+    <div className={styles.header}>
       <div className={styles.headerContainer}>
-        <Image src={appLogo} alt="App Logo" />
+        <Image className={styles.appLogo} src={appLogo} alt="App Logo" />
 
         <div className={styles.navContainer}>
-          {navItems.map((item) => (
-            <Link key={item.name} href={item.link}>
-              {item.name}
-            </Link>
-          ))}
+          {navItems.map(
+            (item) =>
+              !item.hidden && (
+                <Link
+                  key={item.name}
+                  href={item.link}
+                  className={styles.navItem}
+                >
+                  {item.name}
+                </Link>
+              )
+          )}
         </div>
       </div>
-    </header>
+    </div>
   );
 };
 
